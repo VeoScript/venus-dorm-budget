52514c6d5e100a08dca45ff129c0033c04ed8638
Villaruel Jerome <jeromevillaruel1998@gmail.com>
2023-11-30 16:29:30 +0800

    chore: remove convex
    commit 8761191bbf906926c7f830e486dc621f1fdddb9f
    Author: Jerome Villaruel <jeromevillaruel1998@gmail.com>
    Date:   Thu Nov 30 16:28:39 2023 +0800
    
        chore: remove convex (#12)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
 import AppStacks from './src/config/AppStacks';
 
-import {ConvexProvider, ConvexReactClient} from 'convex/react';
-import 'react-native-get-random-values';
-import {CONVEX_URL} from '@env';
-
-const convex = new ConvexReactClient(CONVEX_URL, {
-  unsavedChangesWarning: false,
-});
-
 const App = () => {
-  return (
-    <ConvexProvider client={convex}>
-      <AppStacks />
-    </ConvexProvider>
-  );
+  return <AppStacks />;
 };
 
 export default App;
